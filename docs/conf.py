ca0b2984909c0f7fcdf230159858bfdc874774d3
fsciortino <sciortino@psfc.mit.edu>
2020-11-02 14:01:16 -0500

    attemp 6

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-#sys.path.insert(0, os.path.abspath('../aurora'))
 autodoc_mock_imports = ["aurora"]
 
 
@@ -72,7 +71,7 @@ if not on_rtd: # only import and set the theme if we're buiding docs locally
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = [] #['_static']
 
 
 
