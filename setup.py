81bfcb5c95c10d0a4d03b30c6cbfcae3df19c68d
fsciortino <sciortino@psfc.mit.edu>
2020-12-02 14:32:03 -0500

    update version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ install_requires = open('requirements.txt').read().split('\n')
     
 setup(
       name=package_name,
-      version='1.1.5',
+      version='1.1.6',
       description=long_description,
       long_description_content_type='text/markdown',
       url='https://github.com/fsciortino/Aurora',
