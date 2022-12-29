53c7e350e2e034dec3f5eb958854801d834af8a1
Tomas Odstrcil <32073690+odstrcilt@users.noreply.github.com>
2022-12-29 12:09:19 -0800

    Modify the steady state example

--- examples/steady_state_run.py
+++ examples/steady_state_run.py
@@ -4,19 +4,22 @@ Script to test functionality of steady-state run with AURORA.
 It is recommended to run this in IPython.
 """
 
+
+
 import numpy as np
 import matplotlib.pyplot as plt
+from time import time
 
-plt.ion()
 from omfit_classes import omfit_eqdsk
 import sys, os
 from scipy.interpolate import interp1d
 import copy
-
+plt.ion()
 # Make sure that package home is added to sys.path
 sys.path.append("../")
 import aurora
 
+ 
 # read in default Aurora namelist
 namelist = aurora.default_nml.load_default_namelist()
 
@@ -24,8 +27,10 @@ namelist = aurora.default_nml.load_default_namelist()
 examples_dir = os.path.dirname(os.path.abspath(__file__))
 geqdsk = omfit_eqdsk.OMFITgeqdsk(examples_dir + "/example.gfile")
 
+ 
 # save kinetic profiles on a rhop (sqrt of norm. pol. flux) grid
 # parameterization f=(f_center-f_edge)*(1-rhop**alpha1)**alpha2 + f_edge
+
 kp = namelist["kin_profs"]
 T_core = 5e3  # eV
 T_edge = 100  # eV
@@ -40,44 +45,65 @@ rhop = kp["Te"]["rhop"] = kp["ne"]["rhop"] = np.linspace(0, 1, 100)
 ne = kp["ne"]["vals"] = (n_core - n_edge) * (1 - rhop ** n_alpha1) ** n_alpha2 + n_edge
 Te = kp["Te"]["vals"] = (T_core - T_edge) * (1 - rhop ** T_alpha1) ** T_alpha2 + T_edge
 
+ 
+namelist['rvol_lcfs'] = 70
+namelist['Raxis_cm'] = 170
+namelist['lim_sep'] = 5.6
+namelist['clen_divertor'] = 25
+namelist['clen_limiter'] = 0.5
+namelist['bound_sep'] = 8
+namelist['source_cm_out_lcfs'] = 4.5
+namelist['recycling_switch'] = 0
+namelist['dr_0'] = 1
+namelist['dr_1'] = 0.1
+namelist['K'] = 10
+
+  
 # set impurity species and sources rate to 0
-imp = namelist["imp"] = "Ar"
+imp = namelist["imp"] = 'Ne' 
 namelist["source_type"] = "const"
-namelist["source_rate"] = 1e31  # particles/s
+namelist["source_rate"] = 1e18  # particles/s
+namelist["SOL_mach"] = 0.1
 
-# get charge state distributions from ionization equilibrium
-atom_data = aurora.atomic.get_atom_data(imp, ["scd", "acd"])
-
-# get fractional abundances on ne (cm^-3) and Te (eV) grid
-_Te, fz = aurora.atomic.get_frac_abundances(atom_data, ne, Te, rho=rhop, plot=False)
-
-# initial guess for steady state Ar charge state densities
-nz_init = 1e-25 * ne[:, None] * fz
-
+#use just a single time 
+namelist["timing"]['times'] = [0,1e-6]
+ 
 # Now get aurora setup
-asim = aurora.core.aurora_sim(namelist, geqdsk=geqdsk)
-
-nz_init = interp1d(rhop, nz_init, bounds_error=False, fill_value=0.0, axis=0)(
-    asim.rhop_grid
-)
-
+asim = aurora.core.aurora_sim(namelist )
+ 
 # set time-independent transport coefficients
 D_z = 1e4 * np.ones(len(asim.rvol_grid))  # cm^2/s
 V_z = -10e2 * asim.rhop_grid ** 5  # cm/s
 
+#Z dependent D and V
+#D_z = np.tile(D_z, (asim.Z_imp+1,1)).T
+#V_z = np.tile(V_z, (asim.Z_imp+1,1)).T
+ 
+
+t = time()
+nz_norm_steady = asim.run_aurora_steady_analytic( D_z, V_z)#) ,  rvol,  nz[0], ploss, Sne, Rne, lam = 1.0)
+print('Analytical steady solution calculated in : %.3fs'%(time()-t))
+ 
+ 
+
 n_steps = 10
-max_sim_time = 500e-3
+max_sim_time = 1000
+t = time()
+nz_norm_steady2 = asim.run_aurora_steady(D_z, V_z, nz_init= None,
+                                        tolerance=0.01, max_sim_time = max_sim_time,
+                                        dt=1e-4, dt_increase=1.05,
+                                        n_steps = n_steps,
+                                        plot=False)
+print('Steady solution 2 calculated in : %.3fs'%(time()-t))
 
-nz_norm_steady = asim.run_aurora_steady(
-    D_z,
-    V_z,
-    nz_init=None,  # nz_init,
-    tolerance=0.01,
-    max_sim_time=max_sim_time,
-    dt=1e-4,
-    dt_increase=1.05,
-    n_steps=n_steps,
-    plot=True,
-)
 
-print("tau_imp:", asim.tau_imp)
+
+plt.plot(asim.rhop_btw,nz_norm_steady.T )
+plt.gca().set_prop_cycle(None)
+plt.plot(asim.rhop_grid,nz_norm_steady2[:,1:] , '--')
+plt.plot(asim.rhop_btw,nz_norm_steady.sum(0),'k-',lw=2,label='Analytical solution')
+plt.plot(asim.rhop_grid,nz_norm_steady2.sum(1) ,'k--',lw=2,label='Full iterative solution')
+plt.legend()
+plt.xlabel('rhop')
+plt.ylabel('impurity density [$cm^{-3}$]')
+
