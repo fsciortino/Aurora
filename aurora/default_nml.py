6f2c61d8512ea60e22a7568c8bd823b651a898ae
Antonello Zito <antonellozito94@gmail.com>
2023-01-17 14:07:23 +0100

    Updated default_nml.py

--- aurora/default_nml.py
+++ aurora/default_nml.py
@@ -29,26 +29,12 @@ def load_default_namelist():
     """
 
     namelist = {
-        "imp": "Ca",
-        "main_element": "D",  # background
         # --------------------
-        "source_rate": 1e21,
-        "source_type": "const",
-        # explicit source:
-        "explicit_source_vals": None,  # provide 2D array on explicit_source_time and explicit_source_rhop grids
-        "explicit_source_time": None,
-        "explicit_source_rhop": None,
-        # other options to specify source:
-        "source_width_in": 0.0,  # exponential ionization decay from wall boundary if widths both = 0
-        "source_width_out": 0.0,
-        "imp_source_energy_eV": 3.0,  # only needed if source_width_in=source_width_out=0.0
-        "prompt_redep_flag": False,
-        "source_file": None,  # required if source_type='file'
-        "source_cm_out_lcfs": 1.0,  # source distance in cm from LCFS
-        # LBO synthetic model, only used if source_type='synth_LBO'
-        "LBO": {"n_particles": 1e18, "t_fall": 0.3, "t_rise": 0.05, "t_start": 0.01},
+        # IMPURITY AND MAIN SPECIES
+        "imp": "Ca",  # impurity species
+        "main_element": "D",  # main background species
         # --------------------
-        # time grid specifications
+        # TIME GRID
         "timing": {
             "dt_increase": np.array([1.005, 1.0]),
             "dt_start": np.array([1e-5, 0.001]),
@@ -56,56 +42,107 @@ def load_default_namelist():
             "times": np.array([0.0, 0.1]),
         },
         # --------------------
-        # radial grid
-        "bound_sep": 2.0,
-        "lim_sep": 1.0,
-        "clen_divertor": 17.0,
-        "clen_limiter": 0.5,
+        # RADIAL GRID
+        "K": 6.0,
         "dr_0": 0.3,
         "dr_1": 0.05,
-        "K": 6.0,
+        "lim_sep": 1.0,  # cm
+        "bound_sep": 2.0,  # cm
         "SOL_decay": 0.05,
-        # -------------------
-        "saw_model": {
-            "saw_flag": False,
-            "rmix": 1000.0,
-            "times": [
-                1.0,
-            ],
-            "crash_width": 1.0,
-        },
         # --------------------
-        # edge/recycling
-        "recycling_flag": False,
-        "wall_recycling": 0.0,
-        "tau_div_SOL_ms": 50.0,  # ms
-        "tau_pump_ms": 500.0,  # ms
-        "tau_rcl_ret_ms": 50.0,  # ms
-        "SOL_mach": 0.1,
-        # --------------------
-        # kinetic profiles
+        # KINETIC PROFILES 
         "kin_profs": {
             "ne": {"fun": "interpa", "times": [1.0]},
             "Te": {"fun": "interp", "times": [1.0], "decay": [1.0]},
             "Ti": {"fun": "interp", "times": [1.0], "decay": [1.0]},
             "n0": {"fun": "interpa", "times": [1.0]},
         },
-        # and NBI CXR rates
-        "nbi_cxr": {"rhop": None, "vals": None},  # time indpt profiles; (rhop,nZ)
-        # ----------------
-        # charge exchange flags
-        "cxr_flag": False,
-        "nbi_cxr_flag": False,
-        # -----------------
-        # misc
-        "device": "CMOD",
-        "shot": 99999,
-        "time": 1250,  # in ms, for equilibrium
-        # atomic data (ADF11)
+        # --------------------
+        # ATOMIC PHYSICS
         "acd": None,  # use default
         "scd": None,  # use default
         "ccd": None,  # use default
+        "cxr_flag": False,
+        "nbi_cxr_flag": False,
+        "nbi_cxr": {"rhop": None, "vals": None},  # time indpt profiles; (rhop,nZ)
         "superstages": [],
+        # --------------------
+        # EXTERNAL PARTICLE SOURCES
+        "source_type": "const",
+        "source_rate": 1e21,
+        "source_file": None,  # required if source_type='file'
+        "explicit_source_vals": None,  # provide 2D array on explicit_source_time and explicit_source_rhop grids
+        "explicit_source_time": None,
+        "explicit_source_rhop": None,
+        "LBO": {"n_particles": 1e18, "t_fall": 0.3, "t_rise": 0.05, "t_start": 0.01},
+        # --------------------
+        # NEUTRAL SOURCE PROFILES 
+        "source_width_in": 0.0,  # exponential ionization decay from wall boundary if widths both = 0
+        "source_width_out": 0.0,
+        "source_cm_out_lcfs": 1.0,  # source distance in cm from LCFS   
+        "imp_source_energy_eV": 3.0,  # energy of externally injected impurity neutrals, only needed if source_width_in=source_width_out=0.0
+        "imp_recycling_energy_eV": 3.0,  # energy of recycled impurity neutrals, only needed if source_width_in=source_width_out=0.0
+        "prompt_redep_flag": False,
+        # --------------------
+        # SAWTEETH MODEL
+        "saw_model": {
+            "saw_flag": False,
+            "rmix": 1000.0,
+            "times": [1.0,],
+            "crash_width": 1.0,
+        },
+        # --------------------
+        # EDGE/DIVERTOR
+        "clen_divertor": 17.0, # m
+        "clen_limiter": 0.5, # m
+        "SOL_mach": 0.1,
+        "div_recomb_ratio": 1.0,
+        "recycling_flag": False, 
+        "tau_div_SOL_ms": 50.0,  # ms
+        "screening_eff": 0.0,
+        # --------------------
+        # PLASMA-WALL INTERACTION
+        "wall_recycling": 0.0,
+        "tau_rcl_ret_ms": 50.0,  # ms
+        "phys_surfaces": False,
+        "surf_mainwall": 1.0e5,  # cm^2
+        "surf_divwall": 1.0e4,  # cm^2
+        "mainwall_roughness": 1.0,
+        "divwall_roughness": 1.0,
+        "advanced_PWI": {
+            "advanced_PWI_flag": False,
+            "main_wall_material": 'W',
+            "div_wall_material": 'W',
+            "background_mode": 'manual',
+            "background_species": ['D'],
+            "background_main_wall_fluxes": [0], # s^-1
+            "background_div_wall_fluxes": [0], # s^-1
+            "background_files": ['file/location'],
+            "characteristic_impact_energy_main_wall": 200, # eV
+            "characteristic_impact_energy_div_wall": 500, # eV
+            "n_main_wall_sat": 1e20, # m^-2
+            "n_div_wall_sat": 1e20, # m^-2
+            "energetic_recycled_neutrals": False,
+            "Te_div": 30.0, # eV
+            "Te_lim": 10.0, # eV
+            "Ti_over_Te": 1.0,
+            "gammai": 2.0,
+        },
+        # --------------------
+        # PUMPING
+        "phys_volumes": False,
+        "tau_pump_ms": 500.0,  # ms
+        "vol_div": 1.0e6,  # cm^3
+        "S_pump": 5.0e6,  # cm^3/s
+        "pump_chamber": False,
+        "vol_pump": 1.0e6,  # cm^3
+        "L_divpump": 1.0e7, # cm^3/s
+        "L_leak": 0.0,  # cm^3/s
+        # --------------------
+        # DEVICE
+        "device": "CMOD",
+        "shot": 99999,
+        "time": 1250,  # in ms, for equilibrium
     }
     return namelist
 
