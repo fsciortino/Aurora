9bf644168b82ad40eaeef733e6e5fb276d927d16
cjperks7 <cjperk@mit.edu>
2023-08-03 14:47:29 -0400

    [#85] Moved Gaussian & Lorentzian shape calculator to dedicated functions that can be shared amongst similar broadening mechanisms

--- aurora/line_broaden.py
+++ aurora/line_broaden.py
@@ -66,10 +66,11 @@ def get_line_broaden(
 
 ########################################################
 #
-#                   Utilities
+#             Broadening Mechanisms
 #
 #########################################################
 
+# Calculates Doppler broadening
 def _get_Doppler(
     # Settings
     dpyhsics=None,         # Dictionary of neccessary phyiscs
@@ -93,36 +94,19 @@ def _get_Doppler(
 
     '''
 
-     # Doppler broadening FWHM
+    # Doppler broadening FWHM
     mass = cnt.m_p * dphysics['ion_A'] # [kg]
-    dnu_g = (
+    dnu = (
         np.sqrt(2.0 * (dphysics['Ti_eV'] * cnt.e) / mass)
         / cnt. c
-        * (cnt.c / wave_A)
-        ) # [m/AA * Hz], dim(trs,)
+        * (cnt.c*1e10 / wave_A)
+        ) # [Hz], dim(trs,)
 
-    # set a variable delta lambda based on the width of the broadening
-    _dlam_A = wave_A**2 / cnt.c * dnu_g * 5  # [AA], dim(trs,), 5 standard deviations
-
-    # Wavelength mesh
-    lams_profs_A = np.linspace(
-        wave_A - _dlam_A, 
-        wave_A + _dlam_A, 
-        100, axis=1) # [AA], dim(trs, nlamb)
-
-    # Gaussian profiles of the lines
-    theta = np.exp(
-        -(
-            ((cnt.c / lams_profs_A - cnt.c / wave_A[:, None]) 
-            / dnu_g[:, None]) ** 2
-            )
-        ) # [], dim(trs, nlamb)
-
-    # Normalize Gaussian profile
-    theta /= (
-        np.sqrt(np.pi) * dnu_g[:, None] 
-        * wave_A[:, None] ** 2 / cnt.c
-        ) # [1/AA], dim(trs,nlamb)
+    # Calculates general Gaussian shape
+    lams_profs_A, theta = _get_Gaussian(
+        dnu = dnu,
+        wave_A=wave_A,
+        )
 
     # Output line shape and wavelength mesh
     return {
@@ -130,34 +114,7 @@ def _get_Doppler(
         'theta': theta,                 # [1/AA], dim(trs,nlamb)
         }
 
-
-    # non-equally spaced wavelenght
-    #wave_final_A = np.unique(lams_profs_A)
-
-    #if (plot_all_lines or plot_spec_tot) and ax is None:
-    #    fig, ax = plt.subplots()
-
-    # contributions to spectrum
-    #spec = {}
-    #spec_tot = np.zeros_like(wave_final_A)
-    #colors = {"ioniz": "r", "excit": "b", "recom": "g", "drsat": "m", "chexc": "c"}
-    #for ii in np.arange(lams_profs_A.shape[0]):
-    #    line_shape = interp1d(
-    #        lams_profs_A[ii], theta[ii], bounds_error=False, fill_value=0.0
-    #    )(wave_final_A)
-    #    for typ, intens in line_emiss[ii].items():
-    #        comp = intens * line_shape
-    #        if typ not in spec:
-    #            spec[typ] = np.zeros_like(comp)
-    #
-    #        if plot_all_lines and intens > np.max([l[typ] for l in line_emiss]) / 1000:
-    #            ax.plot(lams_profs_A[ii] + dlam_A, intens * theta[ii], c=colors[typ])
-    #
-    #        spec[typ] += comp
-    #        spec_tot += comp
-    #
-    #return line_shape
-
+# Calculates Natural broadening
 def _get_Natural(
     # Settings
     dphysics=None,
@@ -184,6 +141,7 @@ def _get_Natural(
                     '0.9518': 3.945e14, # [1/s], y line
                     '0.9552': 5.715e9,  # [1/s], z line
                     }
+                    ref: K.M. Aggarwal and F.P. Keenan, 2012 Phys. Scr. 86 035302
 
             wave_A -- [list], dim(trs,), [AA], 
                 transition central wavelength
@@ -225,35 +183,133 @@ def _get_Natural(
 
         # Loop over transitions
         for ii in ind:
-            # set a variable delta lambda based on the width of the broadening
-            _dlam_A = (
-                wave_A[ii]**2 / (cnt.c*1e10) 
-                * dnu * 5  
-                )# [AA], dim(trs,), 5 standard deviations
-
-            # Wavelength mesh
-            lams_profs_A[ii,:] = np.linspace(
-                wave_A[ii] - _dlam_A, 
-                wave_A[ii] + _dlam_A, 
-                100) # [AA], dim(,nlamb)
-
-            # Lorentz profile
-            theta[ii,:] = 1/(
-                1+ (
-                    (1/lam_profs_A[ii,:] - 1/ wave_A[ii])
-                    * cnt.c*1e10
-                    * 2*np.pi*tau
-                    )**2
-                ) # [], dim(,nlamb)
-                
-            # Normalization
-            theta[ii,:] *= 2* tau # [1/Hz]
-
-            # Fixes units
-            theta[ii,:] *= (cnt.c*1e10)/wave_A[ii]**2 # [1/AA]
+            # Calculate Lorentzian shape
+            lams_profs_A[ii,:], theta[ii,:] = _get_Lorentzian(
+                dnu = dnu,
+                wave_A=wave_A[ii],
+                )
 
     # Output line shape and wavelength mesh
     return {
         'lams_profs_A': lams_profs_A,   # [AA], dim(trs,nlamb)
         'theta': theta,                 # [1/AA], dim(trs,nlamb)
         }
+
+# Calculates Instrumental broadening
+def _get_Instru(
+    # Settings
+    dpyhsics=None,         # Dictionary of neccessary phyiscs
+    # ADF15 file
+    wave_A=None,
+    ):
+    '''
+    INPUTS: dphysics -- [dict], necessary physics information
+                i) 'Ti_eV' -- [float], [eV], local ion temperature
+                ii) 'ion_A' -- [float], [amu], species mass
+
+            wave_A -- [list], dim(trs,), [AA], 
+                transition central wavelength
+
+    OUTPUTS: [dict], line shape
+                i) 'lam_profs_A' -- [AA], dim(trs,nlamb), 
+                        wavelength mesh for each transition
+                ii) 'theta' -- [1/AA], dim(trs, nlamb),
+                        line shape for each transition
+
+
+    '''
+
+
+
+########################################################
+#
+#             Line Shapes
+#
+#########################################################
+
+# General Gaussian shape calculator
+def _calc_Gaussian(
+    dnu = None, # [Hz], [float], FWHM
+    wave_A=None, # [AA], dim(trs,), central wavelength
+    ):
+    '''
+    Note this function is meant to be a general-purpose 
+    Gaussian shape calculator to use when considering
+        1) Doppler broadening
+        2) Suprathermal ions
+        3) Instrumental broadening
+
+    '''
+
+    # set a variable delta lambda based on the width of the broadening
+    _dlam_A = (
+        wave_A**2 / (cnt.c*1e10) 
+        * dnu * 5
+        )  # [AA], dim(trs,), 5 standard deviations
+
+    # Wavelength mesh
+    lams_profs_A = np.linspace(
+        wave_A - _dlam_A, 
+        wave_A + _dlam_A, 
+        100, axis=1) # [AA], dim(trs, nlamb)
+
+    # Gaussian profiles of the lines
+    theta = np.exp(
+        -(
+            ((1 / lams_profs_A - 1 / wave_A[:, None])
+            *cnt.c *1e10
+            / dnu[:, None]) ** 2
+            )
+        ) # [], dim(trs, nlamb)
+
+    # Normalize Gaussian profile
+    theta /= (
+        np.sqrt(np.pi) * dnu[:, None] 
+        * wave_A[:, None] ** 2 / (cnt.c*1e10)
+        ) # [1/AA], dim(trs,nlamb)
+
+    # Output
+    return lams_profs_A, theta
+
+# General Lorentzian shape calculator
+def _calc_Lorentzian(
+    dnu = None, # [Hz], [float], FWHM
+    wave_A=None, # [AA], [float], central wavelength
+    ):
+    '''
+    Note this function is meant to be a general-purpose 
+    Lorentzian shape calculator to use when considering
+        1) Natural broadening
+        2) Pressure broadening
+
+    '''
+
+    # set a variable delta lambda based on the width of the broadening
+    _dlam_A = (
+        wave_A**2 / (cnt.c*1e10) 
+        * dnu * 5  
+        )# [AA], dim(trs,), 5 standard deviations
+
+    # Wavelength mesh
+    lams_profs_A = np.linspace(
+        wave_A - _dlam_A, 
+        wave_A + _dlam_A, 
+        100) # [AA], dim(,nlamb)
+
+    # Lorentz profile
+    theta = 1/(
+        1+ (
+            (1/lam_profs_A - 1/ wave_A)
+            * cnt.c*1e10
+            * 2 / dnu
+            )**2
+        ) # [], dim(,nlamb)
+                
+    # Normalization
+    theta *= 2 / (np.pi * dnu) # [1/Hz]
+
+    # Fixes units
+    theta *= (cnt.c*1e10)/wave_A**2 # [1/AA]
+
+    # Output
+    return lams_profs_A, theta
